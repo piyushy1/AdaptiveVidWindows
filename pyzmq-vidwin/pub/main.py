e79b95013a66447d060c02d9b6b26ea1ea245103
piyushy1 <piyushy1@gmail.com>
2020-09-02 23:26:37 +0100

    docker file updated

--- pyzmq-vidwin/pub/main.py
+++ pyzmq-vidwin/pub/main.py
@@ -96,9 +96,10 @@ def publisher(ip="0.0.0.0", port=5551):
     import time
     ctr = 1
 
-    #video_path = '/home/dhaval/piyush/ViIDWIN/Datasets_VIDWIN/test2.mp4'
-    video_path = '/data1/test2.mp4'
+    video_path = '/home/dhaval/piyush/ViIDWIN/Datasets_VIDWIN/test2.mp4'
+    #video_path = '/data1/test2.mp4'
     iframes_list = get_i_frames(video_path)
+    print('The iframe list***************', iframes_list)
     for frame in stream(video_path):
         if ctr in iframes_list:
             batch_input_queue.put([frame,ctr,1])  # an iframe
