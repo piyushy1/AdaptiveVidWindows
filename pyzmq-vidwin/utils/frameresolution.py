0a9631d281fdc53b738c17653528887e1363f857
piyushy1 <piyushy1@gmail.com>
2020-09-24 12:02:06 +0100

    updated frame resolution

--- pyzmq-vidwin/utils/frameresolution.py
+++ pyzmq-vidwin/utils/frameresolution.py
@@ -18,58 +18,69 @@ from PIL import Image
 
 
 # method to print the divisors
-def printDivisors(n) :
-    i = 1
-    while i <= n :
-        if (n % i==0) :
-            print(i)
-        i = i + 1
+def get_resolution_set(aspect_width, aspect_height, resolution):
+    a= resolution[0]
+    b = resolution[1]
+    width =[]
+    height =[]
+    while a >= aspect_width:
+        if (a % aspect_width==0) :
+            width.append(a)
+        a = a - aspect_width
+
+    while b >= aspect_height:
+        if (b % aspect_height==0) :
+            height.append(b)
+        b = b - aspect_height
+
+    resolutionlist = list(zip(width, height))
+    return resolutionlist
 
 
 def read_image_directory(directorypath):
     files = [f for f in listdir(directorypath) if isfile(join(directorypath, f))]
     return files
 
+def prepare_keras_image(img):
+    img = image.load_img(img, target_size=(224, 224))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+    return x
+
 # this function is susceptible to adversial attacks as how IL and OPencv
 # works in reading the images.
-def prepare_cv_image_2_keras_image (img):
+def prepare_cv_image_2_keras_image (img, resolution):
     # convert the color from BGR to RGB then convert to PIL array
     cvt_image =  cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     im_pil = Image.fromarray(cvt_image)
 
     # resize the array (image) then PIL image
-    im_resized = im_pil.resize((224, 224))
+    im_resized = im_pil.resize(resolution)
     img_array = image.img_to_array(im_resized)
     image_array_expanded = np.expand_dims(img_array, axis = 0)
     return preprocess_input(image_array_expanded)
 
+
+# image directory path
+image_directory = '/home/dhaval/piyush/ViIDWIN/Datasets_VIDWIN/voc_validation/car/2008_000027.jpg'
+# get the list of the image files
+image_files = read_image_directory(image_directory)
+# the get the list of the resolution
+resolution_list= get_resolution_set(16,9, (1920,1080))
 #load model
 model = load_model('mobilenet_model_voc.h5')
 
-# image directory path
-#image_directory = '/home/dhaval/piyush/ViIDWIN/Datasets_VIDWIN/voc_validation/car/2008_000027.jpg'
+
+
 image_directory = '/home/dhaval/piyush/ViIDWIN/Datasets_VIDWIN/voc_validation/car/2008_002875.jpg'
-# image_files = read_image_directory(image_directory)
-#
-# for images in image_files:
-#     img = cv2.imread(image_directory+images)
-#     img = cv2.resize(img, (224, 224))
-#     pred = model.predict(img)
-#     print(pred)
 
 
-img = image.load_img(image_directory, target_size=(224, 224))
-x = image.img_to_array(img)
-x= np.expand_dims(x, axis=0)
-x= preprocess_input(x)
+
 
 
 img1 = cv2.imread(image_directory)
 img1 = prepare_cv_image_2_keras_image(img1)
-
-# finally check if it is working
-# if np.array_equal(z, img):
-#     print('TRUE>>>>>>>>>>>>>>>')
 
 pred = model.predict(x)
 pred1 = model.predict(img1)
