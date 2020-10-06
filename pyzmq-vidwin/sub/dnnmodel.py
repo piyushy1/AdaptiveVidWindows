b5f91fa7e790250603ebd6e90e4b12f2723c4eba
piyushy1 <piyushy1@gmail.com>
2020-10-06 23:57:37 +0100

    dnn update

--- pyzmq-vidwin/sub/dnnmodel.py
+++ pyzmq-vidwin/sub/dnnmodel.py
@@ -4,11 +4,13 @@ import numpy as np
 import keras
 from datetime import datetime
 import math
-from keras.models import load_model
-from keras.applications.resnet50 import ResNet50
-from keras.applications.vgg16 import VGG16
-from keras.applications import mobilenet
-from keras.applications.imagenet_utils import decode_predictions
+from tensorflow.keras.models import load_model
+from tensorflow.keras.applications.resnet50 import ResNet50
+from tensorflow.keras.applications.vgg16 import VGG16
+from tensorflow.keras.applications.mobilenet import MobileNet
+from tensorflow.keras.applications.inception_resnet_v2 import InceptionResNetV2, decode_predictions
+#from keras.applications import mobilenet
+#from tensorflow.keras.applications.imagenet_utils import decode_predictions
 
 
 def load_DNN_model(model_name):
@@ -28,9 +30,14 @@ def load_DNN_model(model_name):
         model_VGG = VGG16(weights='imagenet')
         return model_VGG
 
+    # load resnet
+    if model_name == 'InceptionResNet50':
+        model_resnet = InceptionResNetV2(weights='imagenet')
+        return model_resnet
+
     # load mobilenet
     if model_name == 'MobileNet':
-        model_mobilenet = keras.applications.mobilenet.MobileNet()
+        model_mobilenet = MobileNet(weights='imagenet')
         return model_mobilenet
 
     # load mobilenetv2
@@ -41,7 +48,7 @@ def load_DNN_model(model_name):
 # batch holder: function to hold the batch size
 def batch_of_images(frame_list, model):
     #batch_size = 3
-    batch_holder = np.zeros((len(frame_list), 224, 224, 3))
+    batch_holder = np.zeros((len(frame_list), 299, 299, 3))
     i=0
     for frame in frame_list:
         batch_holder[i]= frame[0] # [0]because each frame is attached with time and other metrics
@@ -59,16 +66,16 @@ def batch_prediciton(batch_holder,model):
   dt1 = datetime.now()
   #image = preprocess_input(batch_holder)
   pred = model.predict(batch_holder)
-  print('Predicted:', decode_predictions_custom(pred, 3))
+  #print('Predicted:', decode_predictions_custom(pred, 3))
 
-  # for k in range(0,batch_holder.shape[0]):
-  #     #print(k)
-  #     decode_predictions(pred,k)
+  for k in range(0,batch_holder.shape[0]):
+      #print(k)
+      decode_predictions(pred,k)
 
   # get final time of batch prediction
   dt2 = datetime.now()
   time_diff_batch = dt2-dt1
-  return time_diff_batch.total_seconds()*1000
+  return time_diff_batch.total_seconds()*1000 , pred
 
 
 # decode predictions
